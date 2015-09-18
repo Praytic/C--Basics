23e84a9ee1c7728625b7514e54925c3b95989f51
CHVM <chernogorovvm@gmail.com>
2015-09-18 15:23:22 +0300

    Update Program.cs

--- UniversityStudy/Program.cs
+++ UniversityStudy/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,18 +11,20 @@ namespace UniversityStudy {
 
         static void Main(string[] args) {
 
-			//Part1.Prac4_VI_19();
-			//Part1.Prac4_VII_4();
-			//Part1.Prac5_II_17();
-			//Part1.Prac5_IV_4();
-			//Part1.Prac5_V_2();
+		//Part1.Prac4_VI_19();
+		//Part1.Prac4_VII_4();
+		//Part1.Prac5_II_17();
+		//Part1.Prac5_IV_4();
+		//Part1.Prac5_V_2();
+    		//Part1.Prac6_II_3();
+    		Part1.Prac6_V_6();
 
-			if (args.Length != 0) {
-				Type parts = typeof(Part1);
-				MethodInfo method = parts.GetMethod(args[0]);
-				Part1 tasks = new Part1();
-				method.Invoke(tasks, null);
-			}
+		if (args.Length != 0) {
+			Type parts = typeof(Part1);
+			MethodInfo method = parts.GetMethod(args[0]);
+			Part1 tasks = new Part1();
+			method.Invoke(tasks, null);
+		}
         }
     }
 }
