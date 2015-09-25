4fd16911e826b9b26b9a9128edaaaa650f35e9eb
CHVM <chernogorovvm@gmail.com>
2015-09-25 15:25:43 +0300

    Update Part1.cs

--- UniversityStudy/Part1.cs
+++ UniversityStudy/Part1.cs
@@ -1,10 +1,12 @@
 /* Part1 class contains following tasks:
  * Practice 4 - VI(19), VII(4)
  * Practive 5 - II(17), IV(4), V(2)
- */
-
-/* Part2 class contains following tasks:
+ * 
+ * Part2 class contains following tasks:
  * Practive 6 - II(3), V(6)
+ * 
+ * Part3 class contains following tasks:
+ * Practive 6 - IV(15), VI(2)
  */
 
 using System;
@@ -33,7 +35,8 @@ namespace UniversityStudy {
 
 			for (int i = 0; i < n; i++) {
 				Console.Write("{0} ", arr[i]);
-			}
+            }
+            Console.Write("\n");
 		}
 	}
 
@@ -189,4 +192,68 @@ namespace UniversityStudy {
 			General.WriteArray<int>(a, n);
 		}
 	}
+
+    class Part3 {
+
+        static void ArrayAddArray(int[][] a, ref int n, int m, int[] pasteArray, int number) {
+
+            for (int k = n; k > number; k--) {
+                a[k][] = a[k - 1];
+            }
+            n++;
+            for (int i = 0; i < m; i++) {
+                a[i][number+1] = pasteArray[i];
+            }
+        }
+
+        public static void Prac6_IV_15() {
+
+            int n;
+            int k1, k2;
+            n = General.ReadValue<int>();
+            k1 = General.ReadValue<int>();
+            k2 = General.ReadValue<int>();
+
+            int[][] arr2d = new int[n][];
+            for (int i = 0; i < n; i++) {
+                arr2d[i] = General.ReadArray<int>(n);
+            }
+
+            long[] arrResult = new long[n];
+            for (int i = 0; i < n; i++) {
+                long mul = 1;
+                for (int j = k1 - 1; j < k2; j++) {
+                    mul *= arr2d[j][i];
+                }
+                arrResult[i] = mul;
+            }
+            General.WriteArray<long>(arrResult, n);
+        }
+
+        public static void Prac6_VI_2() {
+
+            int n, m;
+            n = General.ReadValue<int>();
+            m = n;
+            int[] pasteArray = General.ReadArray<int>(n);
+
+            int[][] arr2d = new int[2*n][];
+            for (int i = 0; i < n; i++) {
+                arr2d[i] = General.ReadArray<int>(n);
+            }
+            for (int i = 0; i < n; i++) {
+                bool flag = true;
+                for (int j = 0; j < m; j++) {
+                    if (arr2d[i][j] < 0) flag = false;
+                }
+                if (!flag) {
+                    ArrayAddArray(arr2d, ref n, m, pasteArray, i);
+                    i++;
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                General.WriteArray<int>(arr2d[i], n);
+            }
+        }
+    }
 }
